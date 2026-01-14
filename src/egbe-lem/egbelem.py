6cdb3baab31d4de48d6711237eaa963e64f4e99b
Greg Tucker <gtucker@colorado.edu>
2026-01-14 09:46:18 -0700

    add blank class

--- src/egbe-lem/egbelem.py
+++ src/egbe-lem/egbelem.py
@@ -3,3 +3,12 @@ EGBE-LEM: fluvial landscape evolution model that implements the
 Enhanced Gravel-Bedrock Eroder (EGBE) theory and Landlab component.
 """
 
+
+class EgbeLem:
+    """
+    Implement a fluvial landscape evolution model using 
+    Landlab components, including the EnhancedGravelBedrockEroder.
+    """
+    pass
+
+
